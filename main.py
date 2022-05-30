ae4dd27ddaa3cf45c3197d8444e5e955ff48d712
Isaac <idmather@ucdavis.edu>
2022-05-30 16:36:21 -0700

    working file

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ class MinHeap:
             self._percolate_down(i)
 
 
-def testing_code():
+def testing_code_ints():
+    print('~~~~~Random Ints Check~~~~~')
 
     ARRAY_SIZE = 10000
 
@@ -79,7 +80,7 @@ def testing_code():
         ARRAY_SIZE)]
     unsorted_list2 = copy.deepcopy(unsorted_list)
 
-    print("MinHeap initialization and sorting ...")
+    print("MinHeap initialization and sorting random ints ...")
     start_time = time.perf_counter()
 
     min_heap = MinHeap(unsorted_list)
@@ -88,10 +89,10 @@ def testing_code():
 
     elapsed = stop_time - start_time
 
-    print("Initialization and Sorting ", ARRAY_SIZE, " items took", elapsed,
+    print("Initialization and Sorting ", ARRAY_SIZE, " ints took", elapsed,
           "seconds")
 
-    print("MinHeap Remove and rebalancing")
+    print("MinHeap Remove and rebalancing random ints")
     start_time = time.perf_counter()
 
     unsorted_list = [min_heap.remove() for _ in range(ARRAY_SIZE)]
@@ -100,20 +101,80 @@ def testing_code():
 
     elapsed = stop_time - start_time
 
-    print("Removing and re-sorting", ARRAY_SIZE, "items took", elapsed,
+    print("Removing and re-sorting", ARRAY_SIZE, "random ints took", elapsed,
           "seconds")
 
     print()
-    print("Verifying sort was correct")
+    print("Verifying sort was correct of random ints")
     unsorted_list2.sort()
 
-    print(f'Was the list correctly sorted? True or False? '
+    print(f'Was the random int list correctly sorted? True or False? '
+          f'{unsorted_list==unsorted_list2}\n')
+
+def testing_code_floats():
+    print('~~~~~Random Floats Check~~~~~')
+
+    ARRAY_SIZE = 10000
+
+    unsorted_list = [random.uniform(-10000, 10000) for _ in range(
+        ARRAY_SIZE)]
+    unsorted_list2 = copy.deepcopy(unsorted_list)
+
+    print("MinHeap initialization and sorting random floats ...")
+    start_time = time.perf_counter()
+
+    min_heap = MinHeap(unsorted_list)
+
+    stop_time = time.perf_counter()
+
+    elapsed = stop_time - start_time
+
+    print("Initialization and Sorting ", ARRAY_SIZE, " floats took", elapsed,
+          "seconds")
+
+    print("MinHeap Remove and rebalancing random floats")
+    start_time = time.perf_counter()
+
+    unsorted_list = [min_heap.remove() for _ in range(ARRAY_SIZE)]
+
+    stop_time = time.perf_counter()
+
+    elapsed = stop_time - start_time
+
+    print("Removing and re-sorting", ARRAY_SIZE, "random floats took", elapsed,
+          "seconds")
+    print()
+    print("Verifying sort was correct of random floats")
+    unsorted_list2.sort()
+
+    print(f'Was the random floats list correctly sorted? True or False? '
           f'{unsorted_list==unsorted_list2}')
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    testing_code()
+    testing_code_ints()
+    testing_code_floats()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#sample output code
+# /Users/isaacmather/PycharmProjects/Assignment7/venv/bin/python /Users/isaacmather/PycharmProjects/Assignment7/main.py
+# ~~~~~Random Ints Check~~~~~
+# MinHeap initialization and sorting random ints ...
+# Initialization and Sorting  10000  ints took 0.037031802 seconds
+# MinHeap Remove and rebalancing random ints
+# Removing and re-sorting 10000 random ints took 0.15445794699999998 seconds
+#
+# Verifying sort was correct of random ints
+# Was the random int list correctly sorted? True or False? True
+#
+# ~~~~~Random Floats Check~~~~~
+# MinHeap initialization and sorting random floats ...
+# Initialization and Sorting  10000  floats took 0.019110393000000003 seconds
+# MinHeap Remove and rebalancing random floats
+# Removing and re-sorting 10000 random floats took 0.11726590400000003 seconds
+#
+# Verifying sort was correct of random floats
+# Was the random floats list correctly sorted? True or False? True
+#
+# Process finished with exit code 0
